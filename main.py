c93bfda9ad1bc9f95ee74aa09e7484cf229e326e
no-inspi <71600397+no-inspi@users.noreply.github.com>
2022-06-01 12:32:54 +0200

    link neo4j docker

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 from typing import Union
+from neomodel import StructuredNode, StringProperty, RelationshipTo, RelationshipFrom, config
 
 from fastapi import FastAPI
 
 app = FastAPI()
 
+config.DATABASE_URL = 'bolt://neo4j:test@localhost:7687'
+config.ENCRYPTED_CONNECTION = False
+
+
+class Book(StructuredNode):
+    title = StringProperty(unique_index=True)
+    author = RelationshipTo('Author', 'AUTHOR')
+
+class Author(StructuredNode):
+    name = StringProperty(unique_index=True)
+    books = RelationshipFrom('Book', 'AUTHOR')
+
+
 
 @app.get("/")
 def read_root():
+    rowling =  Author(name='J. K. Rowling').save()
     return {"Hello": "World"}
 
 
